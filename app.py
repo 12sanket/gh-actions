41448f86283444398e14a77e945c276ba2b91e26
Sanket Kumbhar <40975947+12sanket@users.noreply.github.com>
2025-11-17 15:11:39 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+ 
+# Create a Flask application instance
+app = Flask(__name__)
+ 
+# Define a route and a view function
+@app.route("/")
+def hello_world():
+    return "<p>Hello all from Sanket</p>"
+ 
+# Run the application if the script is executed directly
+if __name__ == "__main__":
+    app.run(debug=True)
