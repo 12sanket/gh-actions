2a0fb7bc3c87e0042f4cd7020a957ca8c68bf004
Sanket Kumbhar <40975947+12sanket@users.noreply.github.com>
2025-11-17 16:01:16 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -11,4 +11,4 @@ def client():
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.json == {"message": "Hello all from Sanket"}
+    assert response.json == {"message": "Hello all from Sanket :-)"}
